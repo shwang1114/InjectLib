2ae7f8101ddd1414bc3c3746877399187fd49c6d
QiuChenly <QiuChenly@outlook.com>
2023-10-11 13:35:20 +0800

    FIX: Intel Only FIX Helper Crash

--- tool/surge.sh
+++ tool/surge.sh
@@ -1,21 +1,13 @@
-echo "是否全新安装Surge?"
-echo "这将删除你的默认配置信息.请先备份配置信息到其他位置."
-read -p "(y/n,默认n):" option 
-if [ $option = 'y' ];then             #判断用户是否输入，如果未输入则打印error
-  # declare user=$(whoami)
-  sudo /bin/launchctl unload /Library/LaunchDaemons/com.nssurge.surge-mac.helper.plist
-  # sudo /usr/bin/killall -u root -9 com.nssurge.surge-mac.helper
-  sudo /bin/rm /Library/LaunchDaemons/com.nssurge.surge-mac.helper.plist
-  sudo /bin/rm /Library/PrivilegedHelperTools/com.nssurge.surge-mac.helper
-
-  sudo rm -rf ~/Library/Preferences/com.nssurge.surge-mac.plist
-  sudo rm -rf ~/Library/Application\ Support/com.nssurge.surge-mac
-else
-  echo "非全新安装,跳过清除。"
-fi
+# declare user=$(whoami)
+sudo /bin/launchctl unload /Library/LaunchDaemons/com.nssurge.surge-mac.helper.plist
+sudo /usr/bin/killall -u root -9 com.nssurge.surge-mac.helper
+sudo /bin/rm /Library/LaunchDaemons/com.nssurge.surge-mac.helper.plist
+sudo /bin/rm /Library/PrivilegedHelperTools/com.nssurge.surge-mac.helper
+# sudo rm -rf ~/Library/Preferences/com.nssurge.surge-mac.plist
+sudo rm -rf ~/Library/Application\ Support/com.nssurge.surge-mac
 
 echo "感谢QQ 302****398 用户无偿提供授权信息。"
-echo "大胆！检测到你在用盗版软件，这可能会危害你的设备！甚至被国家安全局和保密处就地正法，请三思！"
+echo "大胆！检测到你在用盗版软件，这可能会危害你的设备！还可能会导致你被有关监管部门或工业和信息化委员会上门约谈，请慎重考虑是否决定使用盗版！"
 
 helper='/Applications/Surge.app/Contents/Library/LaunchServices/com.nssurge.surge-mac.helper'
 
@@ -33,25 +25,30 @@ echo 455c8: 20 00 80 D2 C0 03 5F D6 |sudo xxd -r - "$helper" #arm64
 echo abed: 6A 01 58 C3 |sudo xxd -r - "$helper" #intel
 echo 3dec8: 20 00 80 D2 C0 03 5F D6 |sudo xxd -r - "$helper" #arm64
 
+# 版本2400
+echo aa5d: 6A 01 58 C3 |sudo xxd -r - "$helper" #intel
+echo 3dcec: 20 00 80 D2 C0 03 5F D6 |sudo xxd -r - "$helper" #arm64
+
 echo "定位你的Mac物理地址完成，正在向国家安全局特工发送你的逮捕许可..."
 offsets=$(grep -a -b -o "\x3C\x73\x74\x72\x69\x6E\x67\x3E\x61\x6E\x63\x68\x6F\x72" $helper | cut -d: -f1)
 sed 's/\x0A/\n/g' <<< "$offsets" | while read -r s; do
   declare -i start=$s
-  echo "起始点在 $start,文件已被修改，跳过注入Helper。"
   if [ "$start" -le 0 ]; then
+      echo "起始点在 $start,文件已被修改，跳过注入Helper。"
       break
   fi
-  # <string> 3C 73 74 72 69 6E 67 3E
-  # <string>anchor apple generic and identifier &quot;com.nssurge.surge-mac&quot; and (certificate leaf[field.1.2.840.113635.100.6.1.9] /* exists */ or certificate 1[field.1.2.840.113635.100.6.2.6] /* exists */ and certificate leaf[field.1.2.840.113635.100.6.1.13] /* exists */ and certificate leaf[subject.OU] = &quot;YCKFLA6N72&quot;)</string>
-  # 3C 73 74 72 69 6E 67 3E 61 6E 63 68 6F 72 20 61 70 70 6C 65 20 67 65 6E 65 72 69 63 20 61 6E 64 20 69 64 65 6E 74 69 66 69 65 72 20 26 71 75 6F 74 3B 63 6F 6D 2E 6E 73 73 75 72 67 65 2E 73 75 72 67 65 2D 6D 61 63 26 71 75 6F 74 3B 20 61 6E 64 20 28 63 65 72 74 69 66 69 63 61 74 65 20 6C 65 61 66 5B 66 69 65 6C 64 2E 31 2E 32 2E 38 34 30 2E 31 31 33 36 33 35 2E 31 30 30 2E 36 2E 31 2E 39 5D 20 2F 2A 20 65 78 69 73 74 73 20 2A 2F 20 6F 72 20 63 65 72 74 69 66 69 63 61 74 65 20 31 5B 66 69 65 6C 64 2E 31 2E 32 2E 38 34 30 2E 31 31 33 36 33 35 2E 31 30 30 2E 36 2E 32 2E 36 5D 20 2F 2A 20 65 78 69 73 74 73 20 2A 2F 20 61 6E 64 20 63 65 72 74 69 66 69 63 61 74 65 20 6C 65 61 66 5B 66 69 65 6C 64 2E 31 2E 32 2E 38 34 30 2E 31 31 33 36 33 35 2E 31 30 30 2E 36 2E 31 2E 31 33 5D 20 2F 2A 20 65 78 69 73 74 73 20 2A 2F 20 61 6E 64 20 63 65 72 74 69 66 69 63 61 74 65 20 6C 65 61 66 5B 73 75 62 6A 65 63 74 2E 4F 55 5D 20 3D 20 26 71 75 6F 74 3B 59 43 4B 46 4C 41 36 4E 37 32 26 71 75 6F 74 3B 29 3C 2F 73 74 72 69 6E 67 3E
+  echo "起始点在 $start, 准备修改Helper文件。"
   echo "69 64 65 6E 74 69 66 69 65 72 20 26 71 75 6F 74 3B 63 6F 6D 2E 6E 73 73 75 72 67 65 2E 73 75 72 67 65 2D 6D 61 63 26 71 75 6F 74 3B 3C 2F 73 74 72 69 6E 67 3E" | xxd -r -p | dd of="$helper" bs=1 seek="$((start + 8))" count=53 conv=notrunc
+  # start + 8 适用于 <string>八字节
+  # start + 53 + 8
   start_pos=$((start + 53 + 8))
-  fill_byte="09"
-
+  fill_byte=""
+  lens=0
   for ((i=0;i<341-53-8;i++)); do
-    pos=$((start_pos + i))
-    echo "$fill_byte" | xxd -r -p | dd bs=1 seek=$pos of="$helper" count=1 conv=notrunc
+    lens=$((start_pos + i))
+    fill_byte+="09 "
   done
+  echo "$fill_byte" | xxd -r -p | dd bs=1 seek=$start_pos of="$helper" count=$((lens - 1)) conv=notrunc
 done
 
 echo "下发逮捕许可完成,即将有人来查你的水表，你别急...海内存知己,天涯若比邻.正在黑进你的Mac,目前已成功骗取到用户root密码."
